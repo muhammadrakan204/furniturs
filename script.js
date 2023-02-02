cd2520e7c31e11a9170ec1d9c4cff0b75a22bce5
muhammadrakan204 <rakanmuhammad21@gmail.com>
2023-02-02 22:37:16 +0700

    menambahkan sec 3 dan memberikan animasi jika button header di hover

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 const navbarNav = document.querySelector(".navbar-nav");
-const hamburger = document.querySelector("#hamburger");
+const linkNav = document.querySelectorAll(".navbar-nav .linkNav");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const satu = document.querySelector(".satu");
+const dua = document.querySelector(".dua");
+const tiga = document.querySelector(".tiga");
 
-hamburger.addEventListener("click", function (e) {
-  e.preventDefault();
-  navbarNav.classList.toggle("active");
-  hamburger.classList.toggle("close-hamburger");
-});
+const animationHamburger = () => {
+  hamburgerMenu.addEventListener("click", function (e) {
+    e.preventDefault();
+    satu.classList.toggle("lima");
+    dua.classList.toggle("seratus");
+    tiga.classList.toggle("lima");
+    navbarNav.classList.toggle("active");
+  });
 
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
+  document.addEventListener("click", function (e) {
+    if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+      navbarNav.classList.remove("active");
+      satu.classList.remove("lima");
+      dua.classList.remove("seratus");
+      tiga.classList.remove("lima");
+    }
+  });
+};
+animationHamburger();
+
+// merubah warna bg navlink ketika di hover di ketinggian tertentu
+window.onscroll = function () {
+  changeFont();
+};
+
+const changeFont = () => {
+  const ketinggian = document.documentElement.scrollTop;
+  for (let i = 0; i < linkNav.length; i++) {
+    if (ketinggian > 569) {
+      linkNav[i].addEventListener("mouseover", function () {
+        linkNav[i].style.backgroundColor = "#FFF8EA";
+      });
+      linkNav[i].addEventListener("mouseout", function () {
+        linkNav[i].style.backgroundColor = "";
+      });
+    } else {
+      linkNav[i].addEventListener("mouseover", function () {
+        linkNav[i].style.backgroundColor = "#B1C387";
+      });
+    }
   }
-});
+};
